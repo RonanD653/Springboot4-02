f996752287a58c3cc3ccd1d1cf27daa3b4bcdcd6
RonanD653 <Ronan_D@Live.co.uk>
2026-01-12 20:18:26 +0000

    Initialise

---
+++ src/test/java/com/springboot4/demo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot4.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
