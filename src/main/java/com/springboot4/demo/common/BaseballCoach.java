8eb7202d4a36b6f50bae38a220ffef62aea4f04d
RonanD653 <Ronan_D@Live.co.uk>
2026-01-14 20:15:21 +0000

    06 compete

--- src/main/java/com/springboot4/demo/common/BaseballCoach.java
+++ src/main/java/com/springboot4/demo/common/BaseballCoach.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Component;
 public class BaseballCoach implements Coach {
 
 
+    public BaseballCoach() {
+        System.out.println("In Constructor: " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice batting for 30 minutes!! :)";
